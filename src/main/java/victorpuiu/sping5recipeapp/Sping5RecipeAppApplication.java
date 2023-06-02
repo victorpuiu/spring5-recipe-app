cc658310872df45e31b05ec546bb16902351b07f
victorpuiu <puiu.danut@gmail.com>
2023-06-02 15:00:54 +0300

    initial project

---
+++ src/main/java/victorpuiu/sping5recipeapp/Sping5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package victorpuiu.sping5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sping5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sping5RecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/victorpuiu/sping5recipeapp/Sping5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package victorpuiu.sping5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sping5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
