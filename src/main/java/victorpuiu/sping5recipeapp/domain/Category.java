5fdd5353fdc171aadc13bbce7966ffe51b777f16
victorpuiu <puiu.danut@gmail.com>
2023-06-06 23:04:18 +0300

    added Category Jpa entity.

---
+++ src/main/java/victorpuiu/sping5recipeapp/domain/Category.java
@@ -0,0 +1,31 @@
+package victorpuiu.sping5recipeapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String categoryName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
