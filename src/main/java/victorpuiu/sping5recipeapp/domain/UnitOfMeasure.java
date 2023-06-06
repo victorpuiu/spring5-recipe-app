9b7e0a2ee0de203208a647b3332ceff4854940de
victorpuiu <puiu.danut@gmail.com>
2023-06-06 21:26:45 +0300

    create unidirectional Jpa Relationship between Ingredient and UnitOfMeasure entities.

--- src/main/java/victorpuiu/sping5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/victorpuiu/sping5recipeapp/domain/UnitOfMeasure.java
@@ -11,6 +11,21 @@ public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String uom;
+    private String description;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
