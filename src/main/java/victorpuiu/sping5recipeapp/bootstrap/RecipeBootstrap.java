ca51cdce754e4ff85b8cd2c3fce28e9477d98043
victorpuiu <puiu.danut@gmail.com>
2023-06-08 00:57:26 +0300

    implement UOMs

--- src/main/java/victorpuiu/sping5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/victorpuiu/sping5recipeapp/bootstrap/RecipeBootstrap.java
@@ -1,10 +1,20 @@
 package victorpuiu.sping5recipeapp.bootstrap;
 
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import victorpuiu.sping5recipeapp.domain.Recipe;
+import victorpuiu.sping5recipeapp.domain.UnitOfMeasure;
 import victorpuiu.sping5recipeapp.repositories.CategoryRepository;
 import victorpuiu.sping5recipeapp.repositories.RecipeRepository;
 import victorpuiu.sping5recipeapp.repositories.UnitOfMeasureRepository;
 
-public class RecipeBootstrap {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private final CategoryRepository categoryRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
     private final RecipeRepository recipeRepository;
@@ -15,8 +25,63 @@ public class RecipeBootstrap {
         this.recipeRepository = recipeRepository;
     }
 
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        recipeRepository.saveAll(getRecipes());
+    }
+
+    private List<Recipe> getRecipes() {
+
+        List<Recipe> recipes = new ArrayList<>(2);
+
+        //get UOMs
+        Optional<UnitOfMeasure> eachUomOptional = unitOfMeasureRepository.findByDescription("Each");
+        if (!eachUomOptional.isPresent()){
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        Optional<UnitOfMeasure> tableSpoonUomOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+        if (!tableSpoonUomOptional.isPresent()){
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        Optional<UnitOfMeasure> teaSpoonUomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        if (!teaSpoonUomOptional.isPresent()){
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        Optional<UnitOfMeasure> dashUomOptional = unitOfMeasureRepository.findByDescription("Dash");
+        if (!dashUomOptional.isPresent()){
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        Optional<UnitOfMeasure> pintUomOptional = unitOfMeasureRepository.findByDescription("Pint");
+        if (!pintUomOptional.isPresent()){
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        Optional<UnitOfMeasure> cupsUomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        if (!cupsUomOptional.isPresent()){
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        UnitOfMeasure eachUom = eachUomOptional.get();
+        UnitOfMeasure tableSpoonUom = tableSpoonUomOptional.get();
+        UnitOfMeasure teaSpoonUom  = teaSpoonUomOptional.get();
+        UnitOfMeasure dashUom  = dashUomOptional.get();
+        UnitOfMeasure pintUom  = pintUomOptional.get();
+        UnitOfMeasure cupsUom  = cupsUomOptional.get();
+
+
+        //get categories
 
 
 
+
+
+
+
+        return recipes;
+    }
 
 }
