fc1566f7fa9de6dc0108ad2b072d0bc7554c1e93
victorpuiu <puiu.danut@gmail.com>
2023-06-12 00:02:32 +0300

    added RecipeService interface and RecipeServiceImpl

---
+++ src/main/java/victorpuiu/sping5recipeapp/services/RecipeService.java
@@ -0,0 +1,10 @@
+package victorpuiu.sping5recipeapp.services;
+
+import victorpuiu.sping5recipeapp.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
---
+++ src/main/java/victorpuiu/sping5recipeapp/services/RecipeServiceImpl.java
@@ -0,0 +1,26 @@
+package victorpuiu.sping5recipeapp.services;
+
+import org.springframework.stereotype.Service;
+import victorpuiu.sping5recipeapp.domain.Recipe;
+import victorpuiu.sping5recipeapp.repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImpl implements RecipeService{
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+    }
+}
