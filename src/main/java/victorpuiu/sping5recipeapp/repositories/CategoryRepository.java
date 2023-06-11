efb02f842be2ee54b39da6e53a437d2400b2b523
victorpuiu <puiu.danut@gmail.com>
2023-06-11 20:20:45 +0300

    refactor

--- src/main/java/victorpuiu/sping5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/victorpuiu/sping5recipeapp/repositories/CategoryRepository.java
@@ -6,6 +6,7 @@ import victorpuiu.sping5recipeapp.domain.Category;
 
 import java.util.Optional;
 
+
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
--- src/main/java/victorpuiu/sping5recipeapp/repositories/RecipeRepository.java
+++ src/main/java/victorpuiu/sping5recipeapp/repositories/RecipeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import victorpuiu.sping5recipeapp.domain.Recipe;
 
+
 @Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
