f12dc540e069ce5bc2b50cf22a428e465e86b944
victorpuiu <puiu.danut@gmail.com>
2023-06-24 17:51:55 +0300

    refactor with project lombok

--- src/main/java/victorpuiu/sping5recipeapp/domain/Category.java
+++ src/main/java/victorpuiu/sping5recipeapp/domain/Category.java
@@ -1,42 +1,22 @@
 package victorpuiu.sping5recipeapp.domain;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Set;
 
+@Data
 @Entity
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
+
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
--- src/main/java/victorpuiu/sping5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/victorpuiu/sping5recipeapp/services/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package victorpuiu.sping5recipeapp.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import victorpuiu.sping5recipeapp.domain.Recipe;
 import victorpuiu.sping5recipeapp.repositories.RecipeRepository;
@@ -7,6 +8,7 @@ import victorpuiu.sping5recipeapp.repositories.RecipeRepository;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService{
 
@@ -18,7 +20,7 @@ public class RecipeServiceImpl implements RecipeService{
 
     @Override
     public Set<Recipe> getRecipes() {
-
+        log.debug("I'm in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
