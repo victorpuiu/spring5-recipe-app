ff37b4a3440ff9a08b8779275e34428f2fa7e2c6
victorpuiu <puiu.danut@gmail.com>
2023-06-24 17:58:53 +0300

    refactor Notes to use  project lombok getters and setters .. added @Data annotation

--- src/main/java/victorpuiu/sping5recipeapp/domain/Notes.java
+++ src/main/java/victorpuiu/sping5recipeapp/domain/Notes.java
@@ -1,7 +1,9 @@
 package victorpuiu.sping5recipeapp.domain;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 public class Notes {
 
@@ -14,27 +16,4 @@ public class Notes {
     private String recipeNotes;
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
