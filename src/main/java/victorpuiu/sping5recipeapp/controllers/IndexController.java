8662d93f4b126a89cfc8cdfcd5be5301cf86d2fd
victorpuiu <puiu.danut@gmail.com>
2023-06-24 17:55:30 +0300

    refactor IndexController and RecipeBootstrap to use  project lombok Sl4j

--- src/main/java/victorpuiu/sping5recipeapp/controllers/IndexController.java
+++ src/main/java/victorpuiu/sping5recipeapp/controllers/IndexController.java
@@ -1,10 +1,12 @@
 package victorpuiu.sping5recipeapp.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import victorpuiu.sping5recipeapp.services.RecipeService;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -16,6 +18,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Getting index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
