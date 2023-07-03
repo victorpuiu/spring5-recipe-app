fd371c1baac63e9e59a83800b8f86782d4597928
victorpuiu <puiu.danut@gmail.com>
2023-07-03 20:37:50 +0300

    refactor

--- src/test/java/victorpuiu/sping5recipeapp/Sping5RecipeAppApplicationTestsController.java
+++ src/test/java/victorpuiu/sping5recipeapp/Sping5RecipeAppApplicationTestsController.java
@@ -1,10 +1,13 @@
 package victorpuiu.sping5recipeapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class Sping5RecipeAppApplicationTests {
+@RunWith(SpringRunner.class)
+class Sping5RecipeAppApplicationTestsController {
 
     @Test
     void contextLoads() {
