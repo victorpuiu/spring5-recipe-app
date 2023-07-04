c89a5ba7092876f44066486613a51752cbee3368
victorpuiu <puiu.danut@gmail.com>
2023-07-04 12:31:41 +0300

    added converter UnitOfMeasureCommandToUnitOfMeasure

---
+++ src/test/java/victorpuiu/sping5recipeapp/converters/UnitOfMeasureCommandToUnitOfMeasureTest.java
@@ -0,0 +1,15 @@
+package victorpuiu.sping5recipeapp.converters;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnitOfMeasureCommandToUnitOfMeasureTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void convert() {
+    }
+}
