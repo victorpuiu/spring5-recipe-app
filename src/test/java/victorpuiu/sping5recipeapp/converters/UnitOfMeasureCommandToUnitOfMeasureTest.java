b034d78952bbe56ab246c66f0ade46c616e2bfb3
victorpuiu <puiu.danut@gmail.com>
2023-07-04 14:01:51 +0300

    update  UnitOfMeasureCommandToUnitOfMeasureTest

--- src/test/java/victorpuiu/sping5recipeapp/converters/UnitOfMeasureCommandToUnitOfMeasureTest.java
+++ src/test/java/victorpuiu/sping5recipeapp/converters/UnitOfMeasureCommandToUnitOfMeasureTest.java
@@ -2,15 +2,44 @@ package victorpuiu.sping5recipeapp.converters;
 
 import org.junit.Before;
 import org.junit.Test;
+import victorpuiu.sping5recipeapp.commands.UnitOfMeasureCommand;
+import victorpuiu.sping5recipeapp.domain.UnitOfMeasure;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UnitOfMeasureCommandToUnitOfMeasureTest {
 
+    public static final Long LONG_VALUE = 1L;
+    public static final String DESCRIPTION = "description";
+    UnitOfMeasureCommandToUnitOfMeasure converter;
     @Before
     public void setUp() throws Exception {
+        converter = new UnitOfMeasureCommandToUnitOfMeasure();
+    }
+
+    @Test
+    public void testNullParameter() {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() {
+        assertNotNull(converter.convert(new UnitOfMeasureCommand()));
     }
 
     @Test
     public void convert() {
+        //given
+        UnitOfMeasureCommand command = new UnitOfMeasureCommand();
+        command.setId(LONG_VALUE);
+        command.setDescription(DESCRIPTION);
 
+        //when
+        UnitOfMeasure uom = converter.convert(command);
+
+        //then
+        assertNotNull(uom);
+        assertEquals(LONG_VALUE, uom.getId());
+        assertNotNull(DESCRIPTION, uom.getDescription());
     }
 }
