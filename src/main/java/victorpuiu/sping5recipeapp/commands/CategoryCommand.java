c43020900f99ba2e390c2b87f8fbe5711b92b11d
victorpuiu <puiu.danut@gmail.com>
2023-07-04 11:09:45 +0300

    added CategoryCommand class

---
+++ src/main/java/victorpuiu/sping5recipeapp/commands/CategoryCommand.java
@@ -0,0 +1,15 @@
+package victorpuiu.sping5recipeapp.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryCommand {
+    private Long id;
+    private String description;
+
+
+}
